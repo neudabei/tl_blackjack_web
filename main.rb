927ef58f2366736041ab9b3f6d081133c12851e2
neudabei <rschultzejena@gmail.com>
2015-05-15 10:53:50 +0100

    Added betting, solution 7

--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ set :sessions, true
 
 BLACKJACK_AMOUNT = 21
 DEALER_MIN_HIT = 17
+INITIAL_POT_AMOUNT = 500
 
 helpers do
 
@@ -58,12 +59,14 @@ helpers do
   def winner!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
+    session[:player_pot] = session[:player_pot] + session[:player_bet]
     @success = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
   end
 
   def loser!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
+    session[:player_pot] = session[:player_pot] - session[:player_bet]
     @error = "<strong>#{session[:player_name]} loses.</strong> #{msg}"
   end
 
@@ -89,6 +92,7 @@ get '/' do
 end
 
 get '/new_player' do
+  session[:player_pot] = INITIAL_POT_AMOUNT
   erb :new_player
 end
 
@@ -99,7 +103,25 @@ post '/new_player' do
   end
 
   session[:player_name] = params[:player_name]
-  redirect '/game'  
+  redirect '/bet'  
+end
+
+get '/bet' do
+  session[:player_bet] = nil # this clears/resets the bet amount when player comes back after clicking "play again"
+  erb :bet
+end
+
+post '/bet' do
+  if params[:bet_amount].nil? || params[:bet_amount].to_i == 0
+    @error = "Must make a bet."
+    halt erb :bet
+  elsif params[:bet_amount].to_i > session[:player_pot]
+    @error = "Bet amount cannot be greater than what you have ($#{session[:player_pot]})"
+    halt erb :bet
+  else #happy path
+    session[:player_bet] = params[:bet_amount].to_i
+    redirect '/game'
+  end
 end
 
 get '/game' do
