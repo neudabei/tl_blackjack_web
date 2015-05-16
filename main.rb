a8af6412cb65e6fc9cf70620be0ca99004a4a97b
neudabei <rschultzejena@gmail.com>
2015-05-16 12:39:54 +0100

    Fixed error messages by moving them to the layout.erb

--- main.rb
+++ main.rb
@@ -59,20 +59,20 @@ helpers do
     @play_again = true
     @show_hit_or_stay_buttons = false
     session[:player_pot] = session[:player_pot] + session[:player_bet]
-    @success = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
+    @winner = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
   end
 
   def loser!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
     session[:player_pot] = session[:player_pot] - session[:player_bet]
-    @error = "<strong>#{session[:player_name]} loses.</strong> #{msg}"
+    @loser = "<strong>#{session[:player_name]} loses.</strong> #{msg}"
   end
 
   def tie!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
-    @success = "<strong>It's a tie!</strong> #{msg}"
+    @winner = "<strong>It's a tie!</strong> #{msg}"
   end
 
 end
--- public/application.js
+++ public/application.js
@@ -1,34 +1,42 @@
 $(document).ready(function() {
-
-    $(document).on('click', '#hit_form input', function() {
-    $.ajax({
-      type: 'POST', // this is a hash. We are using POST because it is a form we are highhacking
-      url: '/game/player/hit',
-      //data: {} -> only if we have additional paramaters, not the case in our app
-    }).done(function(msg) {
-      $('#game').replaceWith(msg);
-    });
-    return false;
-  });
-
-    $(document).on('click', '#stay_form input', function() {
-    $.ajax({
-      type: 'POST', 
-      url: '/game/player/stay'
-    }).done(function(msg) {
-      $('#game').replaceWith(msg);
-    });
-      return false;
-    });
-
-    $(document).on('click', '#dealer_hit_form input', function() {
-    $.ajax({
-      type: 'POST', 
-      url: '/game/dealer/hit'
-    }).done(function(msg) {
-      $('#game').replaceWith(msg);
-    });
-      return false;
-    });
-
+  player_hits();
+  player_stays();
+  dealer_hits();
 });
+
+  function player_hits() {
+    $(document).on('click', '#hit_form input', function() {
+      $.ajax({
+        type: 'POST', // this is a hash. We are using POST because it is a form we are highhacking
+        url: '/game/player/hit',
+        //data: {} -> only if we have additional paramaters, not the case in our app
+      }).done(function(msg) {
+        $('#game').replaceWith(msg);
+      });
+      return false;
+    });
+  }
+
+  function player_stays() {
+    $(document).on('click', '#stay_form input', function() {
+      $.ajax({
+        type: 'POST', 
+        url: '/game/player/stay'
+      }).done(function(msg) {
+        $('#game').replaceWith(msg);
+      });
+        return false;
+    });
+  }
+
+  function dealer_hits() {
+    $(document).on('click', '#dealer_hit_form input', function() {
+      $.ajax({
+        type: 'POST', 
+        url: '/game/dealer/hit'
+      }).done(function(msg) {
+        $('#game').replaceWith(msg);
+      });
+        return false;
+    });
+  }
